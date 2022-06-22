ce0bd66d2bbb8b3aef2ae24f7b732f5ad78a8a4f
Coderwe2 <104864120+Coderwe1@users.noreply.github.com>
2022-06-22 16:00:13 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let darkMode = localStorage.getItem('darkMode');
+
+const darkModeToggle= document.querySelector('#toggle');
+
+
+
+
+const enableDarkMode =() =>{
+    document.body.classList.add('darkmode');
+
+    localStorage.setItem('darkMode' , 'enabled');
+} 
+
+const disableDarkMode =() =>{
+    document.body.classList.remove('darkmode');
+
+    localStorage.setItem('darkMode' , null);
+}  
+
+if(darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click',()=>{
+     darkMode = localStorage.getItem('darkMode');
+    if(darkMode !== "enabled"){
+        enableDarkMode();
+
+    } else{
+        disableDarkMode();
+    }
+
+
+})
